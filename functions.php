60f25abd4e81f014b031f718cee5c5040204d475
Anderson Grudtner Martins <anderson@publishpress.com>
2021-02-25 17:26:45 -0300

    Fix author description in the archive page

--- functions.php
+++ functions.php
@@ -29,6 +29,12 @@ function genereratepress_child_the_archive_title( $title ) {
 	return $title;
 }
 
+add_action('init', 'generatepress_child_remove_action', 100);
+
+function generatepress_child_remove_action() {
+    remove_action('generate_after_archive_title', 'generate_do_archive_description');
+}
+
 add_action( 'generate_after_archive_title', 'generatepress_child_do_archive_description' );
 function generatepress_child_do_archive_description() {
 	if ( ! is_author() ) {
@@ -38,6 +44,8 @@ function generatepress_child_do_archive_description() {
 	the_post();
 	$author = get_multiple_authors( 0, true, true );
 	rewind_posts();
+
+    $author = $author[0];
 
 	if ( ! empty( $author->description ) ) {
 		echo '<div class="author-info">' . $author->description . '</div>';  // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
